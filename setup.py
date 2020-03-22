56d74ab886f2a3038e117d7f36cef2d65c0505f6
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2020-03-22 03:01:58 +0000

    add setup for utils/compute_overlap of py3.5

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    ext_modules=cythonize("utils/compute_overlap.pyx"),
+    include_dirs=[numpy.get_include()]
+)
+
+# then run
+# python setup.py build_ext --inplace
